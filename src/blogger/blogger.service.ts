461af10f486a474698f51e3f8417cc7c4ce2ecba
Nickolay <arbuzov.n@f-case.ru>
2022-09-15 02:56:42 +0300

    feat

--- src/blogger/blogger.service.ts
+++ src/blogger/blogger.service.ts
@@ -4,6 +4,8 @@ import { PostService } from '../posts/post.service';
 import { Repository } from 'typeorm';
 import { Blogger } from './blogger.entity';
 import { CreateBloggerDto, UpdateBloggerDto } from './dto/blogger.dto';
+import { QueryBlogDto } from '../commonDTO/query.dto';
+import { queryDefault } from '../constants/constants';
 
 
 @Injectable()
@@ -11,27 +13,87 @@ export class BloggerService {
   constructor(
     @Inject('BLOGGER_REPOSITORY') 
     private readonly bloggerRepository: Repository<Blogger>,
+    @Inject('BLOGGER_REPOSITORY') 
+    private readonly postRepository: Repository<Post>,
   ) {}
 
-  async findAll() {
-    const all = await this.bloggerRepository.find({relations: ['posts']});
-    // TODO: research QueryBuilder
-    /*this.bloggerRepository
-    .createQueryBuilder('b')
-    .innerJoin('b.posts', 'p')
-    .where('p.content = :con', { con: 'a'})
-    .select('')
-    .addSelect('')*/
-    // TODO: automapper
-    return all.map(a => {return {id: a.id.toString(), name: a.name, youtubeUrl: a.youtubeUrl}})
-    //return all
+  async findAllPostsByBlogId(id: string, query: QueryBlogDto) {
+
+    const blog = await this.bloggerRepository.findOne({where: {id: id}})
+
+    if(blog) {
+      const repo = this.bloggerRepository.createQueryBuilder('blog')
+      const pageNumber = query.pageNumber ? +query.pageNumber : +queryDefault.pageNumber
+      const pageSize = query.pageSize ? + +query.pageSize : +queryDefault.pageSize
+      const sortDirection = (query.sortDirection ? query.sortDirection.toLocaleUpperCase() : queryDefault.sortDirection.toLocaleUpperCase()) as 'DESC' | 'ASC'
+      const all = await repo
+        .leftJoinAndSelect('blog.posts', 'posts')
+        .where({id: id})
+        .skip((pageNumber-1) * pageSize)
+        .take(pageSize)
+        .orderBy(`posts.${query.sortBy ? query.sortBy : queryDefault.sortBy}`, sortDirection) // TODO search about sort
+        .getOne()
+      const blog = await repo.where({id: id}).getOne()
+      //TODO: automapper
+      //TODO: property order in returned obj's
+      const returnedPosts = all.posts.slice((pageNumber-1) * pageSize, (pageNumber-1) * pageSize + pageSize).map(a => {
+        return {content: a.content, shortDescription: a.shortDescription, title: a.title, blogId: a.blogId, blogName: a.blogName, createdAt: a.createdAt, id: a.id}
+      })
+      return {
+        pagesCount: Math.ceil(blog.posts.length/pageSize), 
+        page: pageNumber, 
+        pageSize: pageSize, 
+        totalCount: blog.posts.length, 
+        items: returnedPosts
+      }
+    } else {
+      throw new HttpException('Blogger not found', HttpStatus.NOT_FOUND);
+    }
+  }
+
+  async findAll(query: QueryBlogDto) {
+    const repo = this.bloggerRepository.createQueryBuilder('blog')
+    if(query.searchNameTerm) {
+      //repo.where("LOWER(blog.name) like :name", { name: `LOWER(%${query.searchNameTerm}%)` })
+      repo.where("LOWER(blog.name) like :name", { name: `%${query.searchNameTerm.toLowerCase()}%` })
+    }
+    
+    const sortDirection = (query.sortDirection ? query.sortDirection.toLocaleUpperCase() : queryDefault.sortDirection.toLocaleUpperCase()) as 'DESC' | 'ASC'
+
+    const all = await repo
+      .skip((query.pageNumber ? (+query.pageNumber-1) : (+queryDefault.pageNumber-1)) * (query.pageSize ? + +query.pageSize : +queryDefault.pageSize))
+      .take(query.pageSize ? +query.pageSize : +queryDefault.pageSize)
+      .orderBy(`blog.${query.sortBy ? query.sortBy : queryDefault.sortBy}`, sortDirection)
+      .getMany()
+
+    const count = await repo.getCount()
+    //TODO: automapper
+    //TODO: property order in returned obj's
+    const returnedBlogs = all.map(a => {return {name: a.name, youtubeUrl: a.youtubeUrl, createdAt: a.createdAt, id: a.id}})
+    return {
+      pagesCount: Math.ceil(count/(query.pageSize ? + +query.pageSize : +queryDefault.pageSize)), 
+      page: query.pageNumber ? +query.pageNumber : +queryDefault.pageNumber, 
+      pageSize: query.pageSize ? +query.pageSize : +queryDefault.pageSize, 
+      totalCount: count, 
+      // скорее всего связано с различной сортировкой в js и postgresql
+      items: query.sortBy === 'name' ? returnedBlogs.sort((a,b) => a.name > b.name && sortDirection === 'ASC' ? 1 : -1 ) : returnedBlogs
+    }
+  }
+
+  // TODO: need to refactor
+  async findOneForCustomDecorator(id: string) {
+    const donorBlogger = await this.bloggerRepository.findOne({where: {id: id}});
+    if(donorBlogger) {
+      return donorBlogger
+    } else {
+      return null
+    }
   }
 
   async findOne(id: string) {
     const donorBlogger = await this.bloggerRepository.findOne({where: {id: id}});
     if(donorBlogger) {
-      // TODO something with id(number => string)
-      return {...donorBlogger, id: donorBlogger.id.toString()}
+      return donorBlogger
     } else {
       throw new HttpException('Blogger not found', HttpStatus.NOT_FOUND);
     }
@@ -41,19 +103,17 @@ export class BloggerService {
     const newBlogger = new Blogger()
     newBlogger.name = dto.name
     newBlogger.youtubeUrl = dto.youtubeUrl
-
+    let date = new Date
+    newBlogger.createdAt = date.toISOString()
     const blogger = await this.bloggerRepository.insert(newBlogger);
-    // TODO something with id(number => string)
-    return {...newBlogger, id: newBlogger.id.toString()};
+    return newBlogger
   }
 
   async updateBlogger(id: string, dto: UpdateBloggerDto) {
     const donorBlogger = await this.bloggerRepository.findOne({where: {id: id}});
     if(donorBlogger) {
-      // TODO something with id(number => string)
       const newBlogger = {
         ...donorBlogger,
-        id: donorBlogger.id.toString(), 
         name: dto.name,
         youtubeUrl: dto.youtubeUrl,
       } 
--- src/posts/dto/post.dto.ts
+++ src/posts/dto/post.dto.ts
@@ -1,8 +1,8 @@
 import { Transform, TransformFnParams } from "class-transformer";
-import { IsString, Length } from "class-validator";
+import { IsOptional, IsString, IsUUID, Length, Validate } from "class-validator";
+import { BlogIsExistRule, BlogIsExist } from "../../blogger/customValidate";
 
 export class CreatePostDto {
-    
     @Transform(({ value }: TransformFnParams) => value?.trim())
     @IsString()
     @Length(1, 30)
@@ -18,8 +18,15 @@ export class CreatePostDto {
     @Length(1, 1000)
     readonly content: string;
 
-    @IsString()
-    readonly bloggerId: string;
+    @IsUUID()
+    @Validate(BlogIsExistRule)
+    readonly blogId: string;
+}
+
+export class CratePostDtoWithoutBlogId extends CreatePostDto {
+    @IsOptional()
+    @IsUUID()
+    readonly blogId: string;
 }
 
 export class UpdatePostDto extends CreatePostDto {}
